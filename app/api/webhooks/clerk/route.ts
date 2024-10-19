0ee9b9530501e6db62a2cce5ff43a5cc76fb1c2f
ensp1re <ostapuk1318@gmail.com>
2024-10-19 21:41:26 +0300

    trying to fix webhooks route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
-import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
@@ -24,41 +25,30 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occurred -- no svix headers", {
+    return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
 
-  // Get the raw body
-  const rawBody = await req.text();
-  console.log("Raw body:", rawBody);
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
-  // Attempt to parse the JSON payload
-  try {
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const payload: any = JSON.parse(rawBody);
-    console.log(payload);
-  } catch (err) {
-    console.error("Error parsing JSON:", err);
-    return new Response("Invalid JSON", { status: 400 });
-  }
-
-  // Create a new Svix instance with your secret
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
-    evt = wh.verify(rawBody, {
+    evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Verification failed", {
+    return new Response("Error occured", {
       status: 400,
     });
   }
@@ -67,25 +57,25 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  // Handle the different event types
+  // CREATE
   if (eventType === "user.created") {
-    const { email_addresses, image_url, first_name, last_name, username } =
+    const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
     const user = {
-      clerkId: id || "",
+      clerkId: id,
       email: email_addresses[0].email_address,
-      username: username || "",
-      firstName: first_name || "",
-      lastName: last_name || "",
+      username: username!,
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
     const newUser = await createUser(user);
 
-    // Set public metadata if user creation was successful
+    // Set public metadata
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id!, {
+      await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
@@ -95,22 +85,23 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: newUser });
   }
 
+  // UPDATE
   if (eventType === "user.updated") {
-    const { image_url, first_name, last_name, username } = evt.data;
+    const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name || "",
-      lastName: last_name || "",
-      username: username || "",
+      firstName: first_name!,
+      lastName: last_name!,
+      username: username!,
       photo: image_url,
     };
 
- 
-    const updatedUser = await updateUser(id!, user);
+    const updatedUser = await updateUser(id, user);
 
     return NextResponse.json({ message: "OK", user: updatedUser });
   }
 
+  // DELETE
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
@@ -119,8 +110,8 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  console.log(`Webhook with ID: ${id} and type: ${eventType}`);
-  console.log("Webhook body:", rawBody);
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
 }
