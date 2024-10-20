571f1740914e74ed81dc2cfdbe5dc15f306865cc
ensp1re <ostapuk1318@gmail.com>
2024-10-20 13:33:23 +0300

    i hope this is last fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -5,7 +5,9 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+import { buffer } from "micro";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { headers } from "next/headers";
 
 export async function POST(req: Request) {
   try {
@@ -19,17 +21,20 @@ export async function POST(req: Request) {
     }
 
     // Get the Svix headers for verification
-    const svix_id = req.headers.get("svix-id") as string;
-    const svix_timestamp = req.headers.get("svix-timestamp") as string;
-    const svix_signature = req.headers.get("svix-signature") as string;
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
 
     // Validate headers
     if (!svix_id || !svix_timestamp || !svix_signature) {
       return new Response("Error occurred -- no svix headers", { status: 400 });
     }
 
-    // Get the raw body of the request
-    const body = await req.text();
+    // Get the body of the request
+    let payload;
+    payload = await req.json();
+    const body = (await buffer(payload)).toString();
 
     const wh = new Webhook(WEBHOOK_SECRET);
     let evt: WebhookEvent;
